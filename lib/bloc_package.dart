250846b25701a7fe481d861c2f9b0fe9c958d3d3
rodrigo <cantelli.r@gmail.com>
2019-10-19 14:19:37 -0300

    add bloc pattern

--- lib/bloc_package.dart
+++ lib/bloc_package.dart
@@ -1,7 +1,74 @@
 library bloc_package;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
+
+Type _typeOf<T>() => T;
+
+abstract class BaseBloc {
+  void dispose();
+}
+
+class BaseLoadingBloc extends BaseBloc {
+  final _isLoading = BehaviorSubject<bool>.seeded(false);
+  Stream<bool> get isLoadingStream => _isLoading.stream;
+
+  void setLoading(bool isLoading) {
+    _isLoading.sink.add(isLoading);
+  }
+
+  @override
+  void dispose() {
+    _isLoading.close();
+  }
+}
+
+class BlocProvider<T extends BaseBloc> extends StatefulWidget {
+  BlocProvider({
+    Key key,
+    @required this.child,
+    @required this.bloc,
+  }) : super(key: key);
+
+  final Widget child;
+  final T bloc;
+
+  @override
+  _BlocProviderState<T> createState() => _BlocProviderState<T>();
+
+  static T of<T extends BaseBloc>(BuildContext context) {
+    final type = _typeOf<_BlocProviderInherited<T>>();
+    _BlocProviderInherited<T> provider =
+        context.ancestorInheritedElementForWidgetOfExactType(type)?.widget;
+    return provider?.bloc;
+  }
+}
+
+class _BlocProviderState<T extends BaseBloc> extends State<BlocProvider<T>> {
+  @override
+  void dispose() {
+    widget.bloc?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new _BlocProviderInherited<T>(
+      bloc: widget.bloc,
+      child: widget.child,
+    );
+  }
+}
+
+class _BlocProviderInherited<T> extends InheritedWidget {
+  _BlocProviderInherited({
+    Key key,
+    @required Widget child,
+    @required this.bloc,
+  }) : super(key: key, child: child);
+
+  final T bloc;
+
+  @override
+  bool updateShouldNotify(_BlocProviderInherited oldWidget) => false;
 }
